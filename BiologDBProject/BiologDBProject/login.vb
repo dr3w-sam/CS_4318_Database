f2b76845d598a1bd9518d254fab3a1020e800efe
Jacquelyn <40440985+JacquelynJohnson@users.noreply.github.com>
2020-05-19 15:38:18 -0500

    login form db connection
    finished connection and credential validation.
    NOTE: professorID must start with either a 1 or 2. 3-9 are reserved for studentID

--- BiologDBProject/BiologDBProject/login.vb
+++ BiologDBProject/BiologDBProject/login.vb
@@ -12,29 +12,59 @@ Public Class LoginForm
   Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
     'collects data from the text boxes and passes info to database for verification
     'if student id entered, call studentAction; else call profAction
+    If CInt(txtUserID.Text.Substring(0, 1)) > 2 Then
+      Try
+        Using mysqlConn As New MySqlConnection("server=localhost;userid=root;password=Quantum_2020;database=biolog")
+          Using query As New MySqlCommand("select studentID from student where studentID = @test", mysqlConn)
+            query.Parameters.Add("@test", MySqlDbType.String).Value = txtUserID.Text
 
-    Try
-      Using mysqlConn As New MySqlConnection("server=localhost;userid=root;password=Quantum_2020;database=biolog")
-        Using query As New MySqlCommand("select studentID from student where studentID = @test", mysqlConn)
-          query.Parameters.Add("@test", MySqlDbType.String).Value = txtUserID.Text
+            mysqlConn.Open()
+            Dim testID As String = query.ExecuteScalar.ToString
+            If testID = txtUserID.Text Then
+              Using q2 As New MySqlCommand("select studentPassword from student where studentID = @test", mysqlConn)
+                q2.Parameters.Add("@test", MySqlDbType.String).Value = txtUserID.Text
+                Dim testPass As String = q2.ExecuteScalar.ToString
+                If testPass = txtPassword.Text Then
+                  studentAction.Show()
 
-          mysqlConn.Open()
-          Dim testID As String = query.ExecuteScalar.ToString
-          If testID = txtUserID.Text Then
-            studentAction.Show()
-          End If
+                End If
+              End Using
+            End If
 
+          End Using
         End Using
-      End Using
 
+      Catch ex As Exception
+        MessageBox.Show(ex.Message)
+      End Try
 
+    Else
+      Try
+        Using mysqlConn As New MySqlConnection("server=localhost;userid=root;password=Quantum_2020;database=biolog")
+          Using query As New MySqlCommand("select professorID from professor where professorID = @test", mysqlConn)
+            query.Parameters.Add("@test", MySqlDbType.String).Value = txtUserID.Text
 
-    Catch ex As Exception
-      MessageBox.Show(ex.Message)
-    End Try
-    'studentAction.Show()   'switches to student viewing frame
-    'profAction.Show()      'switches to professor frame
+            mysqlConn.Open()
+            Dim testID As String = query.ExecuteScalar.ToString
+            If testID = txtUserID.Text Then
+              Using q2 As New MySqlCommand("select profPassword from professor where professorID = @test", mysqlConn)
+                q2.Parameters.Add("@test", MySqlDbType.String).Value = txtUserID.Text
+                Dim testPass As String = q2.ExecuteScalar.ToString
+                If testPass = txtPassword.Text Then
+                  profAction.Show()
 
+                End If
+              End Using
+            End If
+
+          End Using
+        End Using
+
+      Catch ex As Exception
+        MessageBox.Show(ex.Message)
+      End Try
+
+    End If
     Me.Hide()
 
   End Sub
